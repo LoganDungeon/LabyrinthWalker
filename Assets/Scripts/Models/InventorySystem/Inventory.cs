97f360d066b1bf7d23b034d731cf309e018c9151
Laptop\Jonas <JonasW96@gmx.net>
2016-12-12 20:46:55 +0500

    added inventory systems, have to remove some of this

---
+++ Assets/Scripts/Models/InventorySystem/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// an Inventory is used in something like a Chest, a Player, or an NPC
+public class Inventory {
+
+    // and Inventory Slot is a Space where a single item Type can be stored. 
+    // The Slotsize will determine, how much of a type can be stored in there
+    // TODO: implement SlotZise and maybe create an Object for a Slot
+    public int inventorySlots;
+
+    // creates an Inventory with a certain number of slots
+    public Inventory(int _inventorySlots) {
+        inventorySlots = _inventorySlots;
+    }
+}
---
+++ Assets/Scripts/Models/InventorySystem/Item.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public abstract class Item {
+
+    protected string name;
+    protected string description;
+    protected int id;
+
+
+}
