d971ad5afc9ef86699f119620a3657985dfc945e
Laptop\Jonas <JonasW96@gmx.net>
2017-02-17 22:05:43 +0100

    added initial ChestController

--- Assets/Scripts/Models/InventoySystem/Inventory.cs
+++ Assets/Scripts/Models/InventoySystem/Inventory.cs
@@ -19,32 +19,57 @@ public class Inventory {
         this.Slots = new Item[inventorySlots];
     }
 
-    public void AddItem(Item item) {
+    public Item AddItem(Item item) {
         // for now i have 2 different "kinds" of items: 
         // equipables are not stackable and have lifepoints etc.
         // consumables are stackable (maybe i have to change the name later)
-        if (item is Consumable) {
-            for (int i = 0; i < this.Slots.Length; i++) {
-                if (this.Slots[i] == null || this.Slots[i].Id != item.Id) continue;
+        if(item is Consumable) {
+            // item is stackable, so either the item is already in the inventory or an empty slot is needed.
+            // first check if this item is already in the list
+            for(int i = 0; i < this.Slots.Length; i++) {
+                //if the slot is empty or contains an other item, just go on
+                if(this.Slots[i] == null || this.Slots[i].Id != item.Id || 
+                    (this.Slots[i].Id == item.Id && ((Consumable)this.Slots[i]).Quantity == ((Consumable)this.Slots[i]).MaxQuantity))
+                    continue;
                 // found the inventoryslot which contains the item
+                // if the slot can hold all of the new items, just add them together and return
                 if(((Consumable)this.Slots[i]).Quantity + ((Consumable)item).Quantity <= ((Consumable)item).MaxQuantity) {
-                    ((Consumable)this.Slots[i]).Quantity += ((Consumable) item).Quantity;
+                    ((Consumable)this.Slots[i]).Quantity += ((Consumable)item).Quantity;
+                    // the whole itemStack has added to the inventory, so return an emtpy "remaining item"
+                    return null;
+                }
+                else{
+                    // the inventory is in the list, but it is not possible to add the whole stack
+                    // so fill up the slot to the max and try to add the rest to other slots In the inventory
+                    ((Consumable) item).Quantity = ((Consumable) item).Quantity + ((Consumable) this.Slots[i]).Quantity -
+                                                   ((Consumable) item).MaxQuantity;
+                    ((Consumable) this.Slots[i]).Quantity = ((Consumable) this.Slots[i]).MaxQuantity;
+                    return AddItem(item);
                 }
             }
+            // at this point i know the item is not in the inventory and i need a new (empty) slot
+            for (int i = 0; i < this.Slots.Length; i++) {
+                if (this.Slots[i] != null) continue;
+                // empty slot found -> add the item and return
+                this.Slots[i] = item;
+                // no remaining items
+                return null;
+            }
+            // the inventory is full and the item can not be deployed so return the item
+            return item;
         }
-
-
-        foreach (Item itemInList in this.Slots) {
+        else if(item is Equipable) {
+            // Need an empty inventory slot
+            for (int i = 0; i < this.Slots.Length; i++) {
+                if (this.Slots[i] != null) continue;
+                this.Slots[i] = item;
+                return null;
+            }
+            // inventory is full
+            return item;
         }
-        //if (slots[inventorySlot] == null) {
-        //    slots[inventorySlot] = item;
-        //}
-        //else if (slots[inventorySlot].id == item.id) {
-        //    if (slots[inventorySlot] is Consumable && item is Consumable) {
-        //        if (((Consumable) slots[inventorySlot]).quantity + ((Consumable)item).quantity <= ((Consumable)item).maxQuantity) {
-
-        //        }
-        //    }
-        //}
+        // this shoud not be reached ever
+        Debug.Log("Error: AddItem:" + item.Id);
+        return null;
     }
 }
